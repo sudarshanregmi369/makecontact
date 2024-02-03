461068a6f563fa44607fffabff4782df15bf6875
i-m-sudarshan <regmisudarshan8@gmail.com>
2024-02-03 07:52:55 +0545

    errorhandling

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err,request,response, next)=>{
-    const statusCode = response.statusCode?response.statusCode:500
-    response.json({message:err.message,stackTrace:err.stack})
+    const statusCode = response.statusCode  ? response.statusCode : 500
+    response.json({message:err.message, stackTrace:err.stack})
     };
 module.exports = errorHandler;
