b722bd8d84b46bcb37f2c39511f38979e3e49dea
i-m-sudarshan <regmisudarshan8@gmail.com>
2024-02-05 10:47:11 +0545

    change

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,37 +1,41 @@
+const express = require("express")
+
+const asyncHandler = require("express-async-handler")
+
 // get all the contact
-const getContacts = (request, response)=>{
+const getContacts = asyncHandler(async (request, response)=>{
     response.status(200).json({message:"Get all contact"})
-}
+});
 
 // create new contact
 // post/api request  
-const createContact =(request, response)=>{
+const createContact = asyncHandler(async (request, response)=>{
     console.log("the new respone",request.body)
     const{name,email}= request.body;
     if (!name || !email){
         return response.status(500).json({"messag":"Error"})
-        throw new Error("All fields are mandatory")
+        // throw new Error("All fields are mandatory")
     }
     response.status(200).json({message:"create contact "})
-}
+})
 
 //get contact
 //post /api/contact/id
-const getContact=(request, response)=>{
+const getContact= asyncHandler( async (request, response)=>{
     response.send(`Message updates ${request.params.id}`)
-}
+});
 //update the contact
 //put
-const updateContact =(request, response)=>{
+const updateContact = asyncHandler(async(request, response)=>{
     
     response.status(200).json({message:`update contact ${request.params.id}`})
-}
+});
 
 // contact 
 //post/api/contact /id 
-const deleteContact =(request, response)=>{
+const deleteContact = asyncHandler (async(request, response)=>{
     response.send(`Delete contact  with id:${request.params.id}`);
-}
+});
 module.exports={
     getContacts,
     createContact,
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,44 @@
+const {constant}= require("../constant")
 const errorHandler = (request,response, next)=>{
     const statusCode = response.statusCode  ? response.statusCode : 500
-    response.json({message:error.message, stackTrace:error.stack})
+    switch(statusCode){
+        case constant.VALIDATION_ERROR:{
+            response.json({title:"validation ",
+            message:error.message,
+            stackTrace:error.stack})
+    };
+      break;
+      case constant. NOT_FOUND:{
+        response.json({title:"not Found" ,
+        message:error.message,
+         stackTrace:error.stack})
+      };
+      break;
+      case constant.UNAUTHORIZED:{
+        response.json({title:"Unauthorized" ,
+        message:error.message,
+         stackTrace:error.stack})
+      };
+      break;
+      case constant.FORBIDDEN:{
+        response.json({title:"Request forbidden" ,
+        message:error.message,
+         stackTrace:error.stack})
+      };
+      break;
+      case constant.SERVER_ERROR:{
+        response.json({title:"server error" ,
+        message:error.message,
+         stackTrace:error.stack})
+      };
+      break;
+      default:
+        console.log("no error ");
+        break;
+
+    }
+
+   
+    
     };
 module.exports = errorHandler;
