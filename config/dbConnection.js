5e9070142d5ac97c0d27835edac690b1f924699f
sudarshanregmi369 <132115075+sudarshanregmi369@users.noreply.github.com>
2024-02-08 19:43:56 +0545

    third commit

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,8 @@
-const mangoose = require("mangoose");
+const mongoose = require("mongoose");
 
 const connectDb= async ()=>{
     try{
-        const connect = await mangoose.connect(process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log("Database connected",
         connect.connection.host,
         connect.connection.name 
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+
 const dotenv = require("dotenv").config();
 const errorHandler= require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 
-const app = express();
 connectDb();
+const app = express();
+
 const port = process.env.PORT|| 3000;
 
 app.use(express.json());
